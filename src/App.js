a87169cbc83d68955724e8bb537a385c03b835ea
Jeremy Davidson <jeremyfdavidson@gmail.com>
2020-10-14 13:16:35 -0700

    Added simple hello heading.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import React from "react";
 import "./App.css";
-import {
-  Card,
-  CardContent,
-  Container,
-  Grid,
-  Typography,
-} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
   const classes = useStyles();
   return (
     <div>
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h3" component="h1" gutterBottom>
-          Hello
-        </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Card variant="outlined" className={classes.card}>
-              <CardContent>
-                <Typography variant="h5" component="h2" gutterBottom>
-                  Here is some content
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </Container>
+      <div className={classes.main}>
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   main: {
+    margin: "auto",
     marginTop: theme.spacing(5),
     marginBottom: theme.spacing(8),
-  },
-  card: {
-    marginBottom: theme.spacing(2),
-    borderRadius: "15px",
+    width: "50%",
   },
 }));
 
