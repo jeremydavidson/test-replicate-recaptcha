a234f37ea2fb05ed1ac18801e263bf3097681503
Jeremy <jeremy.davidson@ellucian.com>
2020-10-15 10:12:34 -0700

    Added wait to 9 images test because github ci seems a little slow.

--- cypress/integration/integration.test.js
+++ cypress/integration/integration.test.js
@@ -1,3 +1,6 @@
+
+const WAIT_MILLISECONDS = 2500;
+
 describe("The app", () => {
 
   it("should display a heading", () => {
@@ -7,25 +10,25 @@ describe("The app", () => {
 
   it("should display 9 images", () => {
     cy.visit("http://localhost:3000");
-    cy.get("img").should("have.length", 9);
+    cy.wait(WAIT_MILLISECONDS).get("img").should("have.length", 9);
   });
 
   it("should display success after selecting all keys", () => {
     cy.visit("http://localhost:3000");
-    cy.wait(2500).get("[alt='cats']").click({ multiple: true });
-    cy.wait(2500).contains('Submit').click();
+    cy.wait(WAIT_MILLISECONDS).get("[alt='cats']").click({ multiple: true });
+    cy.wait(WAIT_MILLISECONDS).contains('Submit').click();
     cy.contains("Thanks, you selected all");
   });
 
   it("should display a message when evaluation isn't successful", () => {
     cy.visit("http://localhost:3000");
-    cy.wait(2500).contains('Submit').click();
+    cy.wait(WAIT_MILLISECONDS).contains('Submit').click();
     cy.contains("You still have more");
   });
 
   it("should display message when you are ready to submit", () => {
     cy.visit("http://localhost:3000");
-    cy.wait(2500).get("[alt='cats']").click({ multiple: true });
-    cy.wait(2500).contains('You are ready to submit');
+    cy.wait(WAIT_MILLISECONDS).get("[alt='cats']").click({ multiple: true });
+    cy.wait(WAIT_MILLISECONDS).contains('You are ready to submit');
   });
 });
